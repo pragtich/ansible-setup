6f73de49a72a7f0fc5b24619a88d151dfeb1067e
Joris Pragt <pragtich@gmail.com>
2021-01-12 19:29:37 +0100

    Add comment how to handle after fresh flash

--- examples.sh
+++ examples.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# after first flash of Armbian, ssh into the unit (check IP in router webinterface) root pwd: 1234
 ansible-playbook -i hosts  enable-root.yaml
 ansible-playbook -i hosts -k setup.yaml --ask-vault-pass  # Need to enter both root password and vault password
 ansible-playbook -i hosts configure.yaml --ask-vault-pass
